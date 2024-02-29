cdb342f6a63f3acf5ff4e24ad70389d22ff23241
Ana <n.n.ani.de08@gmail.com>
2024-02-28 18:02:07 -0700

    Prueba nueva instalacion

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,47 @@
 const profileElement= document.querySelector(".profile");
 const profileNameElement= profileElement.querySelector(".profile__name");
 const profileAboutElement= profileElement.querySelector(".profile__description");
-const editProfileButton= document.querySelector(".profile__edit");//boton para popup
-const editPopupElement= document.querySelector(".popup");//secc de popup
-const formElement= document.querySelector(".popup__form");
-const nameInput=formElement.querySelector(".popup__form-name");
-const aboutInput=formElement.querySelector(".popup__form-about");
-const saveButton=document.querySelector(".popup__form-save");
-const closeProfileButton= editPopupElement.querySelector(".popup__close-button");
-function openPopup(){
+const editProfileButton= document.querySelector(".profile__edit");//boton para popup nombre
+const editPopupElement= document.querySelector("#editProfile");//secc de popup
+const formElement= document.querySelector("#editForm"); //los edit son para nombre y acerca de
+const nameInput=formElement.querySelector("#editName");
+const aboutInput=formElement.querySelector("#editAbout");
+const saveButton=document.querySelector("#editSave");
+const closeButton= editPopupElement.querySelector("#editCloseButton");
+
+const addPopupButton= document.querySelector(".profile__add-button");// boton pop up imagenes
+const addPopupElement= document.querySelector("#addImage");
+const addFormElement= document.querySelector("#addForm"); //los edit son para nombre y acerca de
+const titleInput=addFormElement.querySelector("#addTitle");
+const linkInput=addFormElement.querySelector("#addLink");
+const addSaveButton=document.querySelector("#addSave");
+const addCloseButton= addPopupElement.querySelector("#addCloseButton");
+
+function handleOpenEditPopup(){
   editPopupElement.classList.add("popup__opened"); //mandamos a llamar nuestra seccion de popup
   nameInput.value = profileNameElement.textContent; //le damos valor inicial a esos form que digan los nombres actuales
   aboutInput.value = profileAboutElement.textContent;
 }
-function closePopup(){
+function closeEditPopup(){
   editPopupElement.classList.remove("popup__opened");
 }
+// aqui empiezan el de las imgs
+function handleOpenAddPopup(){
+  addPopupElement.classList.add("popup__opened");
+}
+function closeAddPopup(){
+  addPopupElement.classList.remove("popup__opened");
+}
+
 function saveInformation(event){
   event.preventDefault();
   profileNameElement.textContent= nameInput.value;
   profileAboutElement.textContent= aboutInput.value;
-  closePopup();
+  closeEditPopup();
 }
-editProfileButton.addEventListener("click",openPopup);
-closeProfileButton.addEventListener("click",closePopup);
+editProfileButton.addEventListener("click",handleOpenEditPopup);
+closeButton.addEventListener("click",closeEditPopup);
 saveButton.addEventListener("click",saveInformation);
 
+addPopupButton.addEventListener("click",handleOpenAddPopup);
+addCloseButton.addEventListener("click",closeAddPopup);
